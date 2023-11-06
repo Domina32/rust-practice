1a44a4ea084c96c85e9e2b0fdb31634dad56bcc6
Domina Sokol <domina.sokol@gmail.com>
2023-11-06 16:03:03 +0100

    use file for successful outputs

--- src/minigrep/minigrep_main.rs
+++ src/minigrep/minigrep_main.rs
@@ -6,12 +6,12 @@ pub fn minigrep_main() {
     let args: Vec<String> = env::args().collect();
 
     let config = minigrep_lib::Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep_lib::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
